8ae17fe0dc10f63bcb543138199902a908f0797c
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2022-09-27 23:31:52 -0400

    css updates

---
+++ javascript.js
@@ -0,0 +1,30 @@
+const add = function(num1, num2) {
+    return num1 + num2;
+};
+    console.log(add(3, 4));
+
+
+const subtract = function(num1, num2) {
+    return num1 - num2;
+};
+    console.log(subtract(5, 2));
+
+
+const multiply = function(num1, num2) {
+    return num1 * num2;
+};
+
+    console.log(multiply(5, 5));
+
+const divide = function(num1, num2) {
+    return num1 / num2;
+};
+
+    console.log(divide(12, 3));
+
+
+const operate = function(name, num1, num2) {
+    return name(num1,num2);
+};
+
+    console.log(operate(multiply, 3, 9));
