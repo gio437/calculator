ecaecaa8e613444a82845641bdb0facda118312e
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2022-09-29 19:42:35 -0400

    added display number function

--- javascript.js
+++ javascript.js
@@ -33,24 +33,64 @@ const operate = function(name, num1, num2) {
 
     console.log(operate(multiply, 3, 9));
 
+// end of operator functions
 
 
-    function clickNum() {
-        let pad = document.querySelector(".numpad"); //store values in a array w ith querySelectorAll?
-        pad.addEventListener("click", displayValue);
+
+
+    let pad = document.querySelector(".numpad"); //store values in a array with querySelectorAll?
+    pad.addEventListener("click", createValue);
+
+function createValue() {
+    let val = 7
+    displayValue(val);
 }
 
-clickNum();
 
 
+function displayValue(val) {
+    let display = document.querySelector(".text");
+    let number = document.createElement("div");
+    
+
+    display.appendChild(number);
 
 
-    function displayValue() {
-        let display = document.querySelector(".text");
-        
-        let number = document.createElement("div");
-        number.classList.add("number");
-        number.textContent = "9";
-
-        display.appendChild(number);
+    switch(val) {
+        case 7:
+            number.classList.add("number");
+            number.textContent = 7;
+            break;
+        case 8:
+            second = 8;
+            break;
+        case 9:
+            third = 9;
+            break;
+        case 4:
+            fourth = 4;
+            break;
+        case 5:
+            fith = 5;
+            break;
+        case 6:
+            sixth = 6;
+            break;
+        case 1:
+            seventh = 1;
+            break;
+        case 2:
+            eigth = 2;
+            break;
+        case 3:
+            ninth = 3;
+            break;
+        case 0:
+            tenth = 0;
+            break;
     }
+
+  
+}
+
+
