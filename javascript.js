762d73387ab6784f6c192710467a4e05b5184ae4
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2022-09-29 23:53:08 -0400

    added start display number

--- javascript.js
+++ javascript.js
@@ -1,7 +1,3 @@
-let num = 0;
-
-
-
 const add = function(num1, num2) {
     return num1 + num2;
 };
@@ -36,15 +32,115 @@ const operate = function(name, num1, num2) {
 // end of operator functions
 
 
+let start = document.querySelector(".begin");
+start.classList.add(".start");
+start.textContent = "0";
 
 
-    let pad = document.querySelector(".numpad"); //store values in a array with querySelectorAll?
-    pad.addEventListener("click", createValue);
+function startCalc() {
+let num = 0;
 
-function createValue() {
+    if (num == 0) {
+        num++;
+        start.textContent = "";
+    }
+    else {
+        return;
+    }
+}
+
+
+let pad = document.querySelector(".numpad1"); //store values in a array with querySelectorAll?
+    pad.addEventListener("click", createValue1);
+
+function createValue1() {
     let val = 7
     displayValue(val);
+    startCalc()
 }
+
+let pad1 = document.querySelector(".numpad2");
+    pad1.addEventListener("click", createValue2);
+
+function createValue2() {
+    let val = 8
+    displayValue(val);
+    startCalc()
+}
+
+let pad2 = document.querySelector(".numpad3"); 
+    pad2.addEventListener("click", createValue3);
+
+function createValue3() {
+    let val = 9
+    displayValue(val);
+    startCalc()
+}
+
+let pad3 = document.querySelector(".numpad4"); 
+    pad3.addEventListener("click", createValue4);
+
+function createValue4() {
+    let val = 4
+    displayValue(val);
+    startCalc()
+}
+
+let pad4 = document.querySelector(".numpad5"); 
+    pad4.addEventListener("click", createValue5);
+
+function createValue5() {
+    let val = 5
+    displayValue(val);
+    startCalc()
+}
+
+let pad5 = document.querySelector(".numpad6"); 
+    pad5.addEventListener("click", createValue6);
+
+function createValue6() {
+    let val = 6
+    displayValue(val);
+    startCalc()
+}
+
+let pad6 = document.querySelector(".numpad7");
+    pad6.addEventListener("click", createValue7);
+
+function createValue7() {
+    let val = 1
+    displayValue(val);
+    startCalc()
+}
+
+let pad7 = document.querySelector(".numpad8"); 
+    pad7.addEventListener("click", createValue8);
+
+function createValue8() {
+    let val = 2
+    displayValue(val);
+    startCalc()
+}
+
+let pad8 = document.querySelector(".numpad9"); 
+    pad8.addEventListener("click", createValue9);
+
+function createValue9() {
+    let val = 3
+    displayValue(val);
+    startCalc()
+}
+
+
+let pad10 = document.getElementById("zero");
+    pad10.addEventListener("click", createValue11);
+
+function createValue11() {
+    let val = 0
+    displayValue(val);
+    startCalc()
+}
+// end of number click events
 
 
 
@@ -62,31 +158,40 @@ function displayValue(val) {
             number.textContent = 7;
             break;
         case 8:
-            second = 8;
+            number.classList.add("number");
+            number.textContent = 8;
             break;
         case 9:
-            third = 9;
+            number.classList.add("number");
+            number.textContent = 9;
             break;
         case 4:
-            fourth = 4;
+            number.classList.add("number");
+            number.textContent = 4;
             break;
         case 5:
-            fith = 5;
+            number.classList.add("number");
+            number.textContent = 5;
             break;
         case 6:
-            sixth = 6;
+            number.classList.add("number");
+            number.textContent = 6;
             break;
         case 1:
-            seventh = 1;
+            number.classList.add("number");
+            number.textContent = 1;
             break;
         case 2:
-            eigth = 2;
+            number.classList.add("number");
+            number.textContent = 2;
             break;
         case 3:
-            ninth = 3;
+            number.classList.add("number");
+            number.textContent = 3;
             break;
         case 0:
-            tenth = 0;
+            number.classList.add("number");
+            number.textContent = 0;
             break;
     }
 
